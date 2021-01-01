bd674f08379231ef78b2c6847d22032299016d28
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-01-01 19:06:12 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExceptionsTut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double convertedToDouble = Double.Parse(input);
+
+            if (convertedToDouble >= 5)
+            {
+                Console.WriteLine($"The entered number is {convertedToDouble}");
+            }
+            else
+            {
+                throw new ArgumentException(" Entered is invalid number !");
+                // here the system just throws an unhandled exception
+            }
+        }
+    }
+}
